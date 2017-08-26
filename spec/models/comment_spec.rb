2c2a7147864ddf332c9120706313cc6cd5d91778
hoquochai <hoquochaibkdn@gmail.com>
2017-08-26 10:10:45 +0700

    write rspec scope post model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,5 @@ RSpec.describe Comment, type: :model do
 
   context "association" do
     it{is_expected.to belong_to :user}
-    it{is_expected.to belong_to :answer}
-    it{is_expected.to belong_to :post}
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,6 +4,67 @@ RSpec.describe Post, type: :model do
   let(:user){FactoryGirl.create :user}
   let(:topic){FactoryGirl.create :topic}
 
+  let!(:post_x) do
+    FactoryGirl.create :post,
+    user_id: user.id,
+    topic_id: topic.id,
+    count_view: 3,
+    created_at: DateTime.new(2016, 01, 01)
+  end
+
+  let!(:post_y) do
+    FactoryGirl.create :post,
+    user_id: user.id,
+    topic_id: topic.id,
+    count_view: 2,
+    created_at: DateTime.new(2016, 02, 01)
+  end
+
+  let!(:post_z) do
+    FactoryGirl.create :post,
+    user_id: user.id,
+    topic_id: topic.id,
+    count_view: 1,
+    created_at: DateTime.new(2016, 03, 01)
+  end
+
+  let!(:answer) do
+    FactoryGirl.create :answer,
+    user_id: user.id,
+    post_id: post_x.id,
+    created_at: DateTime.new(2016, 01, 01)
+
+    FactoryGirl.create :answer,
+    user_id: user.id,
+    post_id: post_y.id,
+    created_at: DateTime.new(2016, 02, 01)
+
+    FactoryGirl.create :answer,
+    user_id: user.id,
+    post_id: post_z.id,
+    created_at: DateTime.new(2016, 03, 01)
+  end
+
+  let!(:comment) do
+    FactoryGirl.create :comment,
+    user_id: user.id,
+    commentable_id: post_x.id,
+    commentable_type: "Post",
+    created_at: DateTime.new(2016, 01, 01)
+
+    FactoryGirl.create :comment,
+    user_id: user.id,
+    commentable_id: post_y.id,
+    commentable_type: "Post",
+    created_at: DateTime.new(2016, 02, 01)
+
+    FactoryGirl.create :comment,
+    user_id: user.id,
+    commentable_id: post_z.id,
+    commentable_type: "Post",
+    created_at: DateTime.new(2016, 03, 01)
+  end
+
   context "association" do
     it{is_expected.to have_many :comments}
     it{is_expected.to have_many :answers}
@@ -11,9 +72,9 @@ RSpec.describe Post, type: :model do
     it{is_expected.to have_many :clips}
     it{expect have_and_belong_to_many :tags}
 
-    it{is_expected.to belong_to :user}
+    it{expect belong_to :user}
     it{is_expected.to belong_to :work_space}
-    it{is_expected.to belong_to :topic}
+    it{expect belong_to :topic}
   end
 
   context "validates" do
@@ -48,6 +109,64 @@ RSpec.describe Post, type: :model do
     it "is invalid with a nil title" do
       expect(FactoryGirl.build(:post, user_id: user.id, topic_id: topic.id,
         title: nil)).not_to be_valid
+    end
+  end
+
+  describe "scope" do
+    context "get post by topic" do
+      it "fail" do
+        expect(Post.get_post_by_topic(0).count).to eq 0
+      end
+
+      it "success" do
+        expect(Post.get_post_by_topic(topic.id).count).to be > 0
+      end
+    end
+
+    context "newest" do
+      it "fail" do
+        expect(Post.newest).not_to eq([post_x, post_y, post_z])
+      end
+
+      it "success" do
+        expect(Post.newest).to eq([post_z, post_y, post_x])
+      end
+    end
+
+    context "popular" do
+      it "fail" do
+        expect(Post.popular).not_to eq([post_z, post_y, post_x])
+      end
+
+      it "success" do
+        expect(Post.popular).to eq([post_x, post_y, post_z])
+      end
+    end
+
+    context "recently answer" do
+      it "fail" do
+        expect(Post.recently_answer).not_to eq([post_x, post_y, post_z])
+      end
+
+      it "success" do
+        expect(Post.recently_answer).to eq([post_z, post_y, post_x])
+      end
+    end
+
+    context "no answer" do
+      it "success" do
+        expect(Post.no_answer.count).to eq 0
+      end
+    end
+
+    context "recently comment" do
+      it "fail" do
+        expect(Post.recently_answer).not_to eq([post_x, post_y, post_z])
+      end
+
+      it "success" do
+        expect(Post.recently_answer).to eq([post_z, post_y, post_x])
+      end
     end
   end
 end
