152ae2bfc952d6008e3ae70a343360c132ef5d28
vo van danh <danh13t1@gmail.com>
2017-10-02 14:20:21 +0700

    bug-see-notification

--- app/jobs/notification_broadcast_job.rb
+++ app/jobs/notification_broadcast_job.rb
@@ -9,7 +9,7 @@ class NotificationBroadcastJob < ApplicationJob
     ActionCable.server.broadcast "notification_channel_#{notification.user_id}",
       mess: "#{notification.activity.owner.name} #{notification.load_message.first}",
       url: ("/posts/" + notification.load_message.last.to_s + "?noti_id=#{notification.id}"),
-      img: notification.activity.owner.avatar.present? ? notification.activity.owner.avatar.url : "no_avatar.png",
+      img: notification.activity.owner.avatar.present? ? notification.activity.owner.avatar.url : "/assets/no_avatar.png",
       name: notification.activity.owner.name, time: time_ago_in_words(notification.created_at)
   end
 end
