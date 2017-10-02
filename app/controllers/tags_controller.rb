f12ce02a9f45144ec3b8a55c51ae881d3335afd4
tranducquoc <ducquocb2@gmail.com>
2017-10-02 11:15:08 +0700

    show tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,21 @@
 class TagsController < ApplicationController
+  before_action :load_tag, only: :show
+
   def index
     @tags = Tag.top_tags.page(params[:page]).per Settings.paginate_tags
+  end
+
+  def show
+    @tag_popular = Tag.top_tags
+    @posts_tag = Post.by_tags(params[:id]).page(params[:page])
+      .per Settings.paginate_default
+  end
+
+  private
+  def load_tag
+    @tag = Tag.find_by id: params[:id]
+    return if @tag
+      redirect_to :back
+      flash[:danger] = t "can_not_load"
   end
 end
