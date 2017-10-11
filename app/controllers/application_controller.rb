c37e0db3d7425722c845718b8f40b84eed221721
vo van danh <danh13t1@gmail.com>
2017-10-11 16:01:37 +0700

    fix-after_sign_in_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,16 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def after_sign_in_path_for resource
-    session[:before_login_url] if session[:before_login_url].present?
+    session[:before_login_url] || root_path
   end
 
   def authenticate_user
     return if user_signed_in?
+    if request.get?
+      session[:before_login_url] = request.original_url
+    else
+      session[:before_login_url] = request.referer
+    end
     respond_to do |format|
       format.html {redirect_to new_user_session_path}
       format.json {render json: {type: false, not_login: true}}
