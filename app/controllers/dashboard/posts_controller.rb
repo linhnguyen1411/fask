0f6830c2cea6a4d75d8685b85788a5c15c04174c
nvtanh <nvtanh.ptit@gmail>
2017-10-17 14:58:02 +0700

    fix some bugs

--- app/controllers/dashboard/posts_controller.rb
+++ app/controllers/dashboard/posts_controller.rb
@@ -2,7 +2,7 @@ class Dashboard::PostsController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @posts = current_user.posts.page(params[:page]).per Settings.paginate_default
+    @posts = current_user.posts.newest.page(params[:page]).per Settings.paginate_default
     @clips_posts = Post.list_posts_clip(current_user.clips.pluck(:post_id))
       .page(params[:page]).per Settings.paginate_default
   end
