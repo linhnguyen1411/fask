67c45312a2511353311da4ab24b7249bd3d35859
linhnguyen1411 <linhnguyencmu@gmail.com>
2017-11-01 09:59:37 +0700

    edit ajax

--- app/controllers/a_versions_controller.rb
+++ app/controllers/a_versions_controller.rb
@@ -1,43 +1,50 @@
 class AVersionsController < ApplicationController
   before_action :authenticate_user!
   before_action :load_posts, only: :create
-  before_action :check_user, only: :index
+  before_action :check_user_owner, only: [:index, :update]
   before_action :load_accept_version, only: :update
 
   def index
     @version_of_post = AVersion.get_version_post_not_reject(@post.id, @post.class.name)
-    .page(params[:page]).per Settings.paginate_default
+      .page(params[:page]).per Settings.paginate_default
   end
 
- def create
-  version = AVersion.new(user_id: current_user.id,
-    content: version_params[:content], a_versionable: @post)
-  if version.save
-    respond_to do |format|
-      format.html do
-        redirect_to post_path @post
+  def create
+    version = AVersion.new(user_id: current_user.id,
+      content: version_params[:content], a_versionable: @post)
+    if version.save
+      respond_to do |format|
+        format.html do
+          redirect_to post_path @post
+        end
+        format.js
       end
-      format.js
     end
-  end
   end
 
   def update
-    version = AVersion.find_by id: params[:id]
-    success = false
-    if params[:status] == Settings.version.accept
-      if version.update_attributes status: :accept
-        @accept_version.update_attributes status: :improve if @accept_version.present?
-        success = true
-      end
-    else params[:status] == Settings.version.reject
-      if version.update_attributes status: :reject
-        success = true
-      end
-    end
-    respond_to do |format|
-      format.json do
-        render json: {type: success}
+    ActiveRecord::Base.transaction do
+      @version = AVersion.find_by id: params[:id]
+      @success = false
+      if params[:status] == Settings.version.accept
+        if @version.update_attributes status: :accept
+          @accept_version.update_attributes status: :improve if @accept_version.present?
+          @success = true
+        end
+        @version_of_post = AVersion.get_version_post_not_reject(@post.id, @post.class.name)
+         .page(params[:page]).per Settings.paginate_default
+        respond_to do |format|
+          format.js
+        end
+      else params[:status] == Settings.version.reject
+        if @version.update_attributes status: :reject
+          @success = true
+        end
+        respond_to do |format|
+          format.json do
+            render json: {type: @success}
+          end
+        end
       end
     end
   end
@@ -54,7 +61,7 @@ class AVersionsController < ApplicationController
     redirect_to root_path
   end
 
-  def check_user
+  def check_user_owner
     @post = Post.find_by id: params[:post_id]
     redirect_to post_path @post.id if current_user.id != @post.user_id
   end
