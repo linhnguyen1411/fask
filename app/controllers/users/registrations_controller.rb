4d0d973b7c6c59322ba6f569a9410a2f4b667e4b
linhnguyen1411 <linhnguyencmu@gmail.com>
2018-01-22 12:58:17 +0700

    change something anonymous user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
+  before_action :check_anonymous
 
   def update
     if current_user.is_create_by_wsm
@@ -24,5 +25,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     if !current_user.is_create_by_wsm
       params.require(:user).permit :name, :avatar
     end
+  end
+
+  def check_anonymous
+    return if current_user.id != Settings.anonymous_number
+    flash[:danger] = t "anonymous_edit_user"
+    redirect_to root_path
   end
 end
