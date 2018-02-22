16d259c08eb79ae87f691d03e0ecf3875d626e01
Linh Nguyen <linhnguyencmu@gmail.com>
2018-02-22 13:25:01 +0700

    rewrite rspec for new phase

--- app/models/supports/user_support.rb
+++ app/models/supports/user_support.rb
@@ -4,11 +4,11 @@ class Supports::UserSupport
   end
 
   def comments_of_user
-    @user.comments
+    @user.comments.comments_of_post_in_topic_on
   end
 
   def answers_of_user
-    @user.answers
+    @user.answers.includes_post_and_topic_on
   end
 
   def posts_of_user
